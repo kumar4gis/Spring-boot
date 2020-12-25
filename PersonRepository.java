9916d3fbf2cee20bd5120d904cafcf21ad36c8d5
kumar4gis <48723105+kumar4gis@users.noreply.github.com>
2020-12-25 06:12:04 -0500

    Update PersonRepository.java

--- PersonRepository.java
+++ PersonRepository.java
@@ -1,6 +1,8 @@
-package net.codejava;
+package net.codejava.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.codejava.Person;
 
 public interface  PersonRepository extends JpaRepository<Person, Long> {
 
