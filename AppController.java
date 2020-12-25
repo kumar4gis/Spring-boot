b6090036ce12505b73961e7dcf138a87adba4e4d
kumar4gis <48723105+kumar4gis@users.noreply.github.com>
2020-12-25 06:09:26 -0500

    Update AppController.java

--- AppController.java
+++ AppController.java
@@ -1,4 +1,4 @@
-package net.codejava;
+package net.codejava.controller;
 
 import java.util.List;
 
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.codejava.Address;
+import net.codejava.Person;
+import net.codejava.service.PersonService;
+@RestController
 public class AppController {
 
 	 @Autowired
